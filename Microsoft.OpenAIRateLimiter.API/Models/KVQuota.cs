399a4f01ced8f717c0b47f23397d632633ed127d
Preston Hale <prestonhale@microsoft.com>
2023-09-07 20:46:50 -0500

    Added New Properties

--- Microsoft.OpenAIRateLimiter.API/Models/KVQuota.cs
+++ Microsoft.OpenAIRateLimiter.API/Models/KVQuota.cs
@@ -11,7 +11,15 @@ namespace Microsoft.OpenAIRateLimiter.API.Models
         public string ProductName { get; set; }
 
         [JsonPropertyName("amount")]
-        public string Amount { get; set; }   
+        public string Amount { get; set; }
 
+        [JsonPropertyName("tokenAmount")]
+        public int TokenAmount { get; set; } = default!;
+
+        [JsonPropertyName("monthlyAmount")]
+        public int MonthlyAmount { get; set; } = default!;
+
+        [JsonPropertyName("createdTime")]
+        public string CreatedTime { get; set; } = default!;
     }
 }
